61a8734ce6570b0cb8cf90417f31fa1e374d1ffb
edixon897 <edixonpayan5@gmail.com>
2024-07-26 15:50:20 -0500

    css cambios

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, session
+from flask import Flask, render_template, request, redirect, url_for, flash, session, jsonify
 from werkzeug.security import generate_password_hash, check_password_hash
 import sqlite3
 from flask_session import Session
--- static/javascript/buscarColor.js
+++ static/javascript/buscarColor.js
@@ -40,3 +40,54 @@ document.addEventListener('DOMContentLoaded', function() {
         
     }
 });
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const buscador = document.getElementById('buscador');
+    const suggestions = document.getElementById('suggestions');
+    const tablaBody = document.getElementById('tabla_body');
+    const originalTableContent = tablaBody.innerHTML; 
+
+    buscador.addEventListener('input', function () {
+        const query = buscador.value;
+
+        if (query.length > 0) {
+            fetch(`/buscar_color?color=${query}`)
+                .then(response => response.json())
+                .then(data => {
+                    suggestions.innerHTML = '';
+                    data.suggestions.forEach(suggestion => {
+                        const div = document.createElement('div');
+                        div.classList.add('suggestion-item');
+                        div.textContent = suggestion;
+                        suggestions.appendChild(div);
+                    });
+
+                    tablaBody.innerHTML = '';
+                    data.results.forEach(row => {
+                        const tr = document.createElement('tr');
+                        tr.innerHTML = `
+                            <td>${row.color}</td>
+                            <td>${row.seccion}</td>
+                            <td data-label="Hojas">${row.total_hojas}</td>
+                        `;
+                        tablaBody.appendChild(tr);
+                    });
+                })
+                .catch(error => console.error('Error:', error));
+        } else {
+            suggestions.innerHTML = '';
+            tablaBody.innerHTML = originalTableContent;
+            location.reload();
+            return;
+        }
+    });
+
+    suggestions.addEventListener('click', function (e) {
+        if (e.target.classList.contains('suggestion-item')) {
+            buscador.value = e.target.textContent;
+            suggestions.innerHTML = '';
+            buscador.dispatchEvent(new Event('input'));
+        }
+    });
+});
